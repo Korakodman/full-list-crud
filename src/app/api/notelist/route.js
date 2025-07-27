7bd76f7f0352ae8b8ceadd4379f07eb6aba1fce0
Korakod <ice9393th@gmail.com>
2025-07-27 22:29:53 +0700

    test post api

--- src/app/api/notelist/route.js
+++ src/app/api/notelist/route.js
@@ -1,12 +1,38 @@
 import { NextResponse } from "next/server";
-
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*", // หรือใส่ URL ที่ต้องการอนุญาต
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function GET() {
-    const note = {
+    const note = [{
       id: Date.now(),
-      namelist: "test",
+      namelist: "Korakod",
       time: new Date().toLocaleString(),
       color: false,
-    }
+    },
+  {
+      id: Date.now(),
+      namelist: "Sahapham",
+      time: new Date().toLocaleString(),
+      color: false,
+    },
+  {
+      id: Date.now(),
+      namelist: "patson",
+      time: new Date().toLocaleString(),
+      color: false,
+    }]
     return NextResponse.json(note,{ status: 200})
+}
+export async function POST(req) {
+  
+    
+    const {name} = await req.json()
+    const newNote = { id: Date.now() , name}
+    console.log(newNote,"ได้ข้อมูล")
+    return NextResponse.json(newNote,{status:201, headers :corsHeaders})
+  
+   
 }
